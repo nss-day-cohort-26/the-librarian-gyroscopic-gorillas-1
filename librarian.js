cbdfa03ead95ebbbd5a97e3349c2e0ccb69867f8
Jennifer Lawson <jenniferlawson000@gmail.com>
2018-06-13 11:44:22 -0500

    added functions

--- librarian.js
+++ librarian.js
@@ -3,27 +3,33 @@ const Librarian = {
         let randNum = Math.floor(100000000 + Math.random() * 900000000);
         customer.libraryCardNum = randNum
     },
-    checkoutABook: function (book, customer) {
+
+    checkoutABook: function (book) {
         if (book.checkedOut) {
             alert("Denied")
         } else {
-            book.checkedOut = true;
             let date = new Date();
             date.setDate(date.getDate() + 14);
             book.dueDate = date;
-            customer.bookShelf = book
         }
     },
+
     checkABookIn: function (book) {
-        if () {
-
+        let date = new Date();
+        date.setDate()
+        if (book.dueDate < date) {
+            alert("You owe $5");
+            book.dueDate = 0;
         } else {
-
+            book.dueDate = 0;
         }
     },
+
     bookSearch: function () {
 
     }
 }
 
-Librarian.checkoutABook("1984")
+Librarian.checkoutABook(libraryDatabase.books["1984"], Jenn)
+
+
